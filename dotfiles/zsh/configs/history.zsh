9a2e74594e902191fa2c2876ac0ce74b9585dbb9
tbxy09 <tb_xy09@163.com>
2018-02-17 00:53:08 +0800

    correct the history.zh and sync to the previous /data/dotfiles, and add the zsh.local as the oh-my-zsh

--- dotfiles/zsh/configs/history.zsh
+++ dotfiles/zsh/configs/history.zsh
@@ -2,5 +2,9 @@ setopt hist_ignore_all_dups inc_append_history
 HISTFILE=~/.zhistory
 HISTSIZE=4096
 SAVEHIST=4096
-
+HSLINES=$(cat /root/.bash_history_summary|sort -k1|uniq -f1|wc -l)
 export ERL_AFLAGS="-kernel shell_history enabled"
+cat /root/.zhistory|sort -k1|uniq -f1 >> /root/.bash_history_summary
+if [ $HSLINES!=0 ];then 
+   cat /root/.bash_history_summary|sort -k1|uniq -f1 > /root/.bash_history_summary
+fi
