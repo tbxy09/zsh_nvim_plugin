5de00fab59c0e3d73b1b29f7a0721ff504761d6c
tbxy09 <tb_xy09@163.com>
2018-02-24 19:32:26 +0800

    the new redirect way, using subshell will make the completing wipeout issue gone(need to futhter check)

--- dotfiles/zsh/configs/history.zsh
+++ dotfiles/zsh/configs/history.zsh
@@ -5,6 +5,17 @@ SAVEHIST=4096
 HSLINES=$(cat /root/.bash_history_summary|sort -k1|uniq -f1|wc -l)
 export ERL_AFLAGS="-kernel shell_history enabled"
 cat /root/.zhistory|sort -k1|uniq -f1 >> /root/.bash_history_summary
-if [ $HSLINES!=0 ];then 
-   cat /root/.bash_history_summary|sort -k1|uniq -f1 > /root/.bash_history_summary
+if [ $HSLINES!=0 ];then
+   # using subshell
+   echo "$(cat /root/.bash_history_summary|sort -k1|uniq -f1)" > /root/.bash_history_summary
+# problem with original code
+#   cat /root/.bash_history_summary|sort -k1|uniq -f1 > .bash_history_summary
+# other solutions
+#   cat /root/.bash_history_summary|sort -k1|uniq -f1 > redirect_buffer
+#   cat redirect_buffer > /root/.bash_history_summary
+
+# using sponge
+#   cat /root/.bash_history_summary|sort -k1|uniq -f1|sponge .bash_history_summary
+#   check the sponge in session buddy
+# using subshell
 fi
