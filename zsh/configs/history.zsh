8d91ed847587e549ad03131d589fec90daa18927
tbxy09 <tb_xy09@163.com>
2018-03-26 18:23:54 +0800

    change /root to ~

--- zsh/configs/history.zsh
+++ zsh/configs/history.zsh
@@ -2,12 +2,12 @@ setopt hist_ignore_all_dups inc_append_history
 HISTFILE=~/.zhistory
 HISTSIZE=4096
 SAVEHIST=4096
-HSLINES=$(cat /root/.bash_history_summary|sort -k1|uniq -f1|wc -l)
+HSLINES=$(cat ~/.bash_history_summary|sort -k1|uniq -f1|wc -l)
 export ERL_AFLAGS="-kernel shell_history enabled"
-cat /root/.zhistory|sort -k1|uniq -f1 >> /root/.bash_history_summary
+cat ~/.zhistory|sort -k1|uniq -f1 >> ~/.bash_history_summary
 if [ $HSLINES!=0 ];then
    # using subshell
-   echo "$(cat /root/.bash_history_summary|sort -k1|uniq -f1)" > /root/.bash_history_summary
+   echo "$(cat ~/.bash_history_summary|sort -k1|uniq -f1)" > ~/.bash_history_summary
 # problem with original code
 #   cat /root/.bash_history_summary|sort -k1|uniq -f1 > .bash_history_summary
 # other solutions
